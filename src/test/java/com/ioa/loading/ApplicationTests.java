c600289d19521f3b423d134d0d92e6eb8f55fd1b
rajgopal Dey <rajgopaldey.89@gmail.com>
2026-07-22 14:44:55 +0530

    run

--- src/test/java/com/ioa/loading/ApplicationTests.java
+++ src/test/java/com/ioa/loading/ApplicationTests.java
@@ -1,10 +1,12 @@
+package com.ioa.loading;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Disabled("Skipping test for CI pipeline")
-class InfoAppBackendApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
